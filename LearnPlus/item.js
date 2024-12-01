4c85e250ef73501fe916fb42f453515b98585ade
titilola awofeso <awofesotitilola@gmail.com>
2024-12-02 02:06:34 +0400

    Added starter files

--- LearnPlus/item.js
+++ LearnPlus/item.js
@@ -1,42 +1,82 @@
-let items =  [
-    {
-      itemId: 1001,
-      image: 'images/maths.jpg',
-      subject: 'Mathematics',
-      location: 'London',
-      price: 100,
-      space: 10
-    },
-    {
-      itemId: 1002,
-      image: 'images/science.jpg',
-      subject: 'Science',
-      location: 'Manchester',
-      price: 120,
-      space: 10
-    },
-    {
-      itemId: 1003,
-      image: 'images/art.jpg',
-      subject: 'Art',
-      location: 'leichester',
-      price: 120,
-      space: 10
-    },
-    {
-      itemId: 1004,
-      image: 'images/music_class.png',
-      subject: 'Music',
-      location: 'Chester',
-      price: 120,
-      space: 10
-    },
-    {
-      itemId: 1004,
-      image: 'images/music_class.png',
-      subject: 'Music',
-      location: 'Chester',
-      price: 120,
-      space: 10
-    },
-  ]
+let items =[
+  {
+    itemId: 1001,
+    image: "images/maths.jpg",
+    subject: "Mathematics",
+    location: "London",
+    price: 100,
+    space: 10
+  },
+  {
+    itemId: 1002,
+    image: "images/science.jpg",
+    subject: "Science",
+    location: "Manchester",
+    price: 229,
+    space: 10
+  },
+  {
+    itemId: 1003,
+    image: "images/art.jpg",
+    subject: "Art",
+    location: "leichester",
+    price: 486,
+    space: 10
+  },
+  {
+    itemId: 1004,
+    image: "images/socialstudies.jpeg",
+    subject: "Social Studies",
+    location: "Chester",
+    price: 60,
+    space: 10
+  },
+  {
+    itemId: 1005,
+    image: "images/french.jpeg",
+    subject: "French",
+    location: "New Castle",
+    price: 180,
+    space: 10
+  },
+  {
+    itemId: 1006,
+    image: "images/programming.jpeg",
+    subject: "Programming for kids",
+    location: "Bradford",
+    price: 860,
+    space: 10
+  },
+  {
+    itemId: 1007,
+    image: "images/history.jpeg",
+    subject: "History",
+    location: "Winchester",
+    price: 240,
+    space: 10
+  },
+  {
+    itemId: 1008,
+    image: "images/english.jpeg",
+    subject: "English",
+    location: "Sheffield",
+    price: 610,
+    space: 10
+  },
+  {
+    itemId: 1009,
+    image: "images/physical.jpeg",
+    subject: "Physical Education",
+    location: "Yorkshire",
+    price: 170,
+    space: 10
+  },
+  {
+    itemId: 1010,
+    image: "images/music_class.png",
+    subject: "Music",
+    location: "Chester",
+    price: 285,
+    space: 10
+  }
+]
--- LearnPlus_backend/server.js
+++ LearnPlus_backend/server.js
@@ -1,17 +1,17 @@
-var express = require("express");
-let app = express();
-app.set('json spaces', 4);
-const cors = require("cors");
-app.use(cors());
+// var express = require("express");
+// let app = express();
+// app.set('json spaces', 4);
+// const cors = require("cors");
+// app.use(cors());
 
 
-let myItem = require('./item.js');
+// let myItem = require('../LearnPlus/item.js');
 
-app.use((req, res) => {
-    res.status(404).send("Resource not found");
-  }); 
+// app.use((req, res) => {
+//     res.status(404).send("Resource not found");
+//   }); 
 
 
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+//   app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+//   });
