59bba18d12168543beb0445ff0e65a5f6c9fd752
titilola awofeso <awofesotitilola@gmail.com>
2024-12-01 21:30:40 +0400

    new commits

---
+++ LearnPlus_backend/server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+let app = express();
+app.set('json spaces', 4);
+const cors = require("cors");
+app.use(cors());
+
+
+let myItem = require('./item.js');
+
+app.use((req, res) => {
+    res.status(404).send("Resource not found");
+  }); 
+
+
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
