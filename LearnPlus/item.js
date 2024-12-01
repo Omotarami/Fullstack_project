35deecdd5254941c64e12cb6ab34b3b808e82258
titilola awofeso <awofesotitilola@gmail.com>
2024-12-02 00:34:04 +0400

    new

---
+++ LearnPlus/item.js
@@ -0,0 +1,42 @@
+let items =  [
+    {
+      itemId: 1001,
+      image: 'images/maths.jpg',
+      subject: 'Mathematics',
+      location: 'London',
+      price: 100,
+      space: 10
+    },
+    {
+      itemId: 1002,
+      image: 'images/science.jpg',
+      subject: 'Science',
+      location: 'Manchester',
+      price: 120,
+      space: 10
+    },
+    {
+      itemId: 1003,
+      image: 'images/art.jpg',
+      subject: 'Art',
+      location: 'leichester',
+      price: 120,
+      space: 10
+    },
+    {
+      itemId: 1004,
+      image: 'images/music_class.png',
+      subject: 'Music',
+      location: 'Chester',
+      price: 120,
+      space: 10
+    },
+    {
+      itemId: 1004,
+      image: 'images/music_class.png',
+      subject: 'Music',
+      location: 'Chester',
+      price: 120,
+      space: 10
+    },
+  ]
